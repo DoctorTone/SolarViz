be3de206650e875be12227dcad21314145ed8fd8
DoctorTone <tony@drt-software.com>
2026-08-12 02:32:33 +0100

    Added parcel data

---
+++ src/state/parcelData.ts
@@ -0,0 +1,15 @@
+export const pvParcels = [
+  {
+    id: "Scopwick Low Field Farm",
+    field_code: "Lf04",
+    boundary: [
+      [508020, 359475],
+      [508060, 359250],
+      [508175, 359200],
+      [508410, 359240],
+    ],
+    panel_top_height: 3.0,
+    tilt_deg: 28,
+    row_pitch_m: 6,
+  },
+];
