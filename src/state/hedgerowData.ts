a5bc6d0042d8896acca3de9c07d69378c3d56364
DoctorTone <tony@drt-software.com>
2026-08-20 18:17:58 +0100

    Reformat hedgerow data per hedge

--- src/state/hedgerowData.ts
+++ src/state/hedgerowData.ts
@@ -1,6 +1,6 @@
 export const hedgerows = [
   {
-    id: "Scopwick Low Field Farm",
+    id: "Scopwick Low - West",
     type: "hedgerow",
     augments_existing: true,
     start_height: 1.8, // existing hedge height at year 0 (m)
@@ -8,14 +8,114 @@ export const hedgerows = [
     points: [
       [507961, 359664],
       [508067, 359272],
+    ],
+  },
+  {
+    id: "Scopwick Low - South West",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
+      [508067, 359272],
+      [508168, 359213],
+    ],
+  },
+  {
+    id: "Scopwick Low - South",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
       [508168, 359213],
       [508435, 359258],
+    ],
+  },
+  {
+    id: "Scopwick Low - South East",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
+      [508435, 359258],
+      [508680, 359435],
+    ],
+  },
+  {
+    id: "Scopwick Low - East",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
       [508680, 359435],
       [508488, 359741],
+    ],
+  },
+  {
+    id: "Scopwick Low - North East 1",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
+      [508488, 359741],
+      [508464, 359736],
+    ],
+  },
+  {
+    id: "Scopwick Low - North East 2",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
       [508464, 359736],
       [508448, 359741],
+    ],
+  },
+  {
+    id: "Scopwick Low - North East 3",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
+      [508448, 359741],
+      [508422, 359732],
+    ],
+  },
+  {
+    id: "Scopwick Low - North East 4",
+    type: "hedgerow",
+    augments_existing: true,
+    start_height: 1.8, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
       [508422, 359732],
       [508338, 359734],
+    ],
+  },
+  {
+    id: "Scopwick Low - North 1",
+    type: "hedgerow",
+    augments_existing: false,
+    start_height: 0, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
+      [508338, 359734],
+      [508170, 359695],
+    ],
+  },
+  {
+    id: "Scopwick Low - North 2",
+    type: "hedgerow",
+    augments_existing: false,
+    start_height: 0, // existing hedge height at year 0 (m)
+    mature_height: 3.5, // documented year-10 maintained height
+    points: [
       [508170, 359695],
       [507961, 359664],
     ],
