b47c1a2d25cbe1a87cf916a1f9afb968987d0343
DoctorTone <tony@drt-software.com>
2026-09-02 00:30:47 +0100

    Remove info icon option

--- src/UI/UI.tsx
+++ src/UI/UI.tsx
@@ -1,12 +1,10 @@
 import Copyright from "../UI/Copyright";
-import Info from "./Info";
 import ViewpointUI from "./ViewpointUI";
 
 const UI = () => {
   return (
     <>
       <Copyright />
-      <Info />
       <ViewpointUI />
     </>
   );
