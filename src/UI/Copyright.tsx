e85ef99bcd462fdb1208c30a63724369ff39edc6
DoctorTone <tony@drt-software.com>
2026-09-02 16:06:26 +0100

    Turn copyright into a link

--- src/UI/Copyright.tsx
+++ src/UI/Copyright.tsx
@@ -1,12 +1,22 @@
 import CopyrightIcon from "@mui/icons-material/Copyright";
+import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 
 const Copyright = () => {
   return (
     <div id="copyright" className="panel">
-      <Typography variant="h6" sx={{ color: "orange", display: "flex" }}>
-        <CopyrightIcon fontSize="large" sx={{ mr: 1 }} /> DRT Software Ltd. 2026
-      </Typography>
+      <Link
+        href="https://www.drt-software.com/"
+        underline="none"
+        target="_blank"
+        rel="noopener"
+        aria-label="Go to DRT software main site"
+      >
+        <Typography variant="h6" sx={{ color: "orange", display: "flex" }}>
+          <CopyrightIcon fontSize="large" sx={{ mr: 1 }} /> DRT Software Ltd.
+          2026
+        </Typography>
+      </Link>
     </div>
   );
 };
